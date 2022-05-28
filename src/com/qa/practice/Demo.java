ba5029948f6d9012f028555186d8a6111641a9e0
mayur-12345 <shirotemayur12345@gmail.com>
2022-05-28 10:50:23 +0530

    Second change added a2 method

--- src/com/qa/practice/Demo.java
+++ src/com/qa/practice/Demo.java
@@ -5,10 +5,14 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("Demo1 ");
 		a1();
+		a2();
 	}
 
 	public static void a1() {
 		System.out.println("a1");
-
+	}
+	
+	public static void a2() {
+		System.out.println("a2");
 	}
 }
