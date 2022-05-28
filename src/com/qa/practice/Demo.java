377f8f3ff179f9eba9d0d7a48a19c6f29de4e8f8
mayur-12345 <shirotemayur12345@gmail.com>
2022-05-28 10:46:01 +0530

    first change

---
+++ src/com/qa/practice/Demo.java
@@ -0,0 +1,14 @@
+package com.qa.practice;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		System.out.println("Demo1 ");
+		a1();
+	}
+
+	public static void a1() {
+		System.out.println("a1");
+
+	}
+}
